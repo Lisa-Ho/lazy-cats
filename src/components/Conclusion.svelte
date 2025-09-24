5107300569ae8f6d3cd57de922181e2268378962
Lisa-Ho <lisa_hornung@gmx.de>
2025-09-24 15:54:58 +0100

    added conclusion section

---
+++ src/components/Conclusion.svelte
@@ -0,0 +1,39 @@
+<script>
+</script>
+
+<div class="section-wrapper">
+    <div class="content-container-column">
+        <h2>So all cats are lazy?</h2>
+        <div class="text-wrapper">
+            <p>
+                Does your cat spend 18 hours a day sleeping? Considering the
+                findings from this study, your cat would not be alone. The good
+                news? What looks like laziness is usually quite normal cat
+                behavior.
+            </p>
+        </div>
+        <div class="text-wrapper">
+            <p>
+                Paragraph on energy saving, ... differences in breeds and by
+                individual cat. 
+                
+                Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate
+                velit esse cillum dolore eu fugiat nulla pariatur. 
+            </p>
+        </div>
+        <div class="text-wrapper">
+            <p>
+                However, sometimes the laziness can stem from a lack of
+                stimulation. Cats are naturally curious and active animals, but
+                if they don’t have enough toys or activities to keep them
+                occupied, they can become bored and appear lethargic. Without
+                proper mental stimulation, cats will often sleep more than
+                usual, which gives off the impression that they’re lazy.
+            </p>
+        </div>
+    </div>
+</div>
