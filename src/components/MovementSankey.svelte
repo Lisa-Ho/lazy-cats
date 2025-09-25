b109bd2046d2410add7d7292a086f84f830c0bb0
Lisa-Ho <lisa_hornung@gmx.de>
2025-09-25 09:37:35 +0100

    adjusted sankey labels and colors

--- src/components/MovementSankey.svelte
+++ src/components/MovementSankey.svelte
@@ -2,20 +2,34 @@
     import { LayerCake, Svg } from "layercake";
 	import Sankey from "./Sankey.svelte";
 	import seasonalData from "../assets/sankey-data.json"
-    //import data from "../assets/sankey-test.js";
 
-	const selectedSeason = 'winter'; 
+	const selectedSeason = 'summer'; 
 	const data = seasonalData[selectedSeason];
 
-    
+    const nodeColors = {
+        "Sitting" : "#53B8CC",
+        "Lying" : "#BDD568",
+        "Standing" : "#F58255" ,
+        "Sitting2" : "#53B8CC",
+        "Lying2" : "#BDD568",
+        "Standing2" : "#F58255" ,
+        "Eating2" : "#D63C7C",
+        "Sitting3" : "#53B8CC",
+        "Lying3" : "#BDD568",
+        "Standing3" : "#F58255" ,
+        "Eating3" : "#D63C7C",
+        "Grooming3" : "#888888",
+    }
+
 </script>
 
 <div class="wrapper"> 
 <div class="chart-container">
     <LayerCake data={data}>
-        <Svg>
+        <Svg>   
             <Sankey
-				nodeWidth=15
+				nodeWidth=20
+                colorNodes={d => nodeColors[d.id]}
             />
         </Svg>
     </LayerCake>
--- src/components/Sankey.svelte
+++ src/components/Sankey.svelte
@@ -2,20 +2,20 @@
 	@component
 	Generates an SVG Sankey chart using [d3-sankey](https://github.com/d3/d3-sankey).
  -->
- <script>
-	import { getContext } from 'svelte';
-	import * as Sankey from 'd3-sankey';
+<script>
+	import { getContext } from "svelte";
+	import * as Sankey from "d3-sankey";
 
-	const { data, width, height } = getContext('LayerCake');
+	const { data, width, height } = getContext("LayerCake");
 
 	/** @type {Function} [colorLinks=d => 'rgba(0, 0, 0, .2)'] – A function to return a color for the links. */
-	export let colorLinks = d => 'rgba(0, 0, 0, .2)';
+	export let colorLinks = (d) => "rgba(0, 0, 0, .2)";
 
 	/** @type {Function} [colorNodes=d => '#333'] – A function to return a color for each node. */
-	export let colorNodes = d => '#333';
+	export let colorNodes = (d) => "#333";
 
 	/** @type {Function} [colorText=d => '#263238'] – A function to return a color for each text label. */
-	export let colorText = d => '#263238';
+	export let colorText = (d) => "#263238";
 
 	/** @type {Number} [nodeWidth=5] – The width of each node, in pixels, passed to [`sankey.nodeWidth`](https://github.com/d3/d3-sankey#sankey_nodeWidth). */
 	export let nodeWidth = 5;
@@ -27,7 +27,7 @@
 	export let linkSort = null;
 
 	/** @type {Function} [nodeId=d => d.id] – The ID field accessor, passed to [`sankey.nodeId`](https://github.com/d3/d3-sankey#sankey_nodeId). */
-	export let nodeId = d => d.id;
+	export let nodeId = (d) => d.id;
 
 	/** @type {Function} [nodeAlign=d3.sankeyLeft] – An alignment function to position the Sankey blocks. See the [d3-sankey documentation](https://github.com/d3/d3-sankey#alignments) for more. */
 	export let nodeAlign = Sankey.sankeyLeft;
@@ -47,42 +47,83 @@
 	$: fontSize = $width <= 320 ? 8 : 12;
 </script>
 
-<style>
-	text {
-		pointer-events: none;
-	}
-</style>
-
 <g class="sankey-layer">
-	<g class='link-group'>
-		{#each sankeyData.links as d}
+	<defs>
+		{#each sankeyData.links as d, i}
+			<linearGradient
+				id={"gradient-" + i}
+				x1="0%"
+				y1="0%"
+				x2="100%"
+				y2="0%"
+				gradientUnits="userSpaceOnUse"
+			>
+				<stop offset="0%" stop-color={colorNodes(d.source) || "#ccc"}  />
+				<stop offset="100%" stop-color={colorNodes(d.target) || "#ccc"} />
+			</linearGradient>
+		{/each}
+	</defs>
+	<g class="link-group">
+		{#each sankeyData.links as d, i}
 			<path
 				d={link(d)}
-				fill='none'
-				stroke={colorLinks(d)}
-				stroke-opacity='0.5'
-				stroke-width={d.width} />
+				fill="none"
+				stroke={`url(#gradient-${i})`}
+				stroke-opacity="0.5"
+				stroke-width={d.width}
+			/>
 		{/each}
 	</g>
-	<g class='rect-group'>
+	<g class="rect-group">
 		{#each sankeyData.nodes as d, i}
-		{console.log(d)}
+			{console.log(d)}
 			<rect
 				x={d.x0}
 				y={d.y0}
 				height={d.y1 - d.y0}
 				width={d.x1 - d.x0}
-				fill={colorNodes(d)} />
+				fill={colorNodes(d)}
+			/>
 			<text
-                class="sankey-label"
+				class="sankey-label"
 				x={d.x0 < $width / 4 ? d.x1 + 6 : d.x0 - 6}
-				y={(d.y1 + d.y0) / 2}
-				dy="{(fontSize / 2) - 2}"
-				style="fill: {colorText(d)};
-							font-size: {fontSize}px;
-							text-anchor: {d.x0 < $width / 4 ? 'start' : 'end'};">
+				y={(d.y1 + d.y0) /2}
+				dy={fontSize / 2 - 6}
+				style="text-anchor: {d.x0 < $width / 4 ? 'start' : 'end'};"
+			>
 				{d.label}
+			</text>
+			<text
+				class="sankey-data-value"
+				x={d.x0 < $width / 4 ? d.x1 - 18 : d.x0 + 9}
+				y={(d.y1 + d.y0) /2}
+				dy={fontSize / 2 -6}
+				style="text-anchor: {d.x0 < $width / 4 ? 'start' : 'middle'};"
+			>
+				{d.value}
 			</text>
 		{/each}
 	</g>
 </g>
+
+<style>
+	text {
+		pointer-events: none;
+	}
+
+	.sankey-label {
+		font-size: 1rem;
+		font-family: "Open Sans", sans-serif;
+		font-weight: 400;
+		dominant-baseline: middle;
+		fill: #222222;
+	}
+
+	.sankey-data-value {
+		font-size: 0.9rem;
+		font-family: "Open Sans", sans-serif;
+		font-weight: 600;
+		dominant-baseline: middle;
+		fill: #222222;
+	}
+</style>
